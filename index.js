22e35e9ca9743ab7562df5337aae49e66c8db499
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-04-26 20:01:49 +0600

    mongodb reinstall

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express'); 
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-const port = process.env.PORT || 3000
-require('dotenv').config()
 const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
 
-// jsfjlsjf
-app.use(express.json())
+
+// middleware
 app.use(cors())
+app.use(express.json())
 
-console.log(process.env.DB_USER);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lb51cqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lb51cqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,26 +22,31 @@ const client = new MongoClient(uri, {
   }
 });
 
-const database = client.db("craftDB")
-const craftItems = database.collection("craftItem")
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/craftitems", (req, res)=>{
+        const newItems= req.body
+        console.log(newItems);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get("/", (req, res)=>{
-    res.send("data will coming soon")
+
+
+app.get("/", (req , res)=>{
+    res.send('data will be coming soon')
 })
+
 app.listen(port, ()=>{
-    console.log(`This server is running on port:${port}`);
+    console.log(`This server runnig on port ${port}`);
 })
